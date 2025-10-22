6c4881d635a931cc67ceccd49f28efd62cc3fd30
koto-thing <gotoukenta62@gmail.com>
2025-10-22 19:09:57 +0900

    とりあえず、音量調整のみ作成 あとThirdPartyフォルダにFMOD SDKをいれておくこと

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿/**
+ *  @file main.cpp
+ *  @author Goto Kenta
+ *  @brief Main entry point for the application.
+ */
+
+# include <iostream>
+# include "./ThirdParty/inc/fmod.hpp"
+
+int main (int argc, char* argv[]) {
+    std::cout << "Hello World!" << std::endl;
+}
